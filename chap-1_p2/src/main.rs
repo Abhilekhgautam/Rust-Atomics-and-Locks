f56e637d28b0736545a2935be07220cb9e8c418e
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-05-25 23:45:22 +0545

    interior mutability using Cell<T>

--- chap-1_p2/src/main.rs
+++ chap-1_p2/src/main.rs
@@ -44,6 +44,32 @@ fn changed_main_two() {
     .unwrap();
 }
 
-fn main() {
-    //todo
+// Rust's borrowing rule can be slightly limiting, especially when dealing with multiple threads
+// but we have a work around for that, interior mutablity
+
+// A Cell<T> wraps a T, but allows mutation through shared reference
+// Getting a borrow off a value of a Cell is not a easy task
+
+// Cell<T> is not thread safe
+#[allow(unused)]
+fn changed_main_three() {
+    use std::cell::Cell;
+
+    // a is immutable
+    let a = Cell::new(2);
+    println!("Before Mutating: {a:?}");
+    // but we can mutate it
+    a.set(3);
+    println!("After Mutating: {a:?}");
+
+    // Cell<T> is not easy to deal with as it doesn't allow to directly borrow the value it holds
+
+    // a_borrowed holds the value held by a previously.
+    let mut a_borrowed = a.take(); // a is set to Default::default()
+    println!("a is now : {a:?}");
+    println!("a previously held: {a_borrowed}");
+    a_borrowed = 5;
+    a.set(a_borrowed); // set a to a new value
 }
+
+fn main() {}
